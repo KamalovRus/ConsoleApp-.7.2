0c8a5a29d221afc41210652e7f725686d42e0f1e
KamalovRus <kamrus97@mail.ru>
2021-11-24 19:30:28 +0500

    Добавьте файлы проекта.

---
+++ ConsoleAppЗадача.7.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppЗадача._7._1
+{
+	class Program
+	{
+		static double Ploshadiob(int a, out int per)
+		{
+			per = 6 * a * a;
+			return Math.Pow(a, 3);
+		}
+		public static void Main()
+		{
+			Console.WriteLine("Введите длинну ребра");
+			int a = Convert.ToInt32(Console.ReadLine());
+			int per = 0;
+			Console.WriteLine("Площадь = {0}, Периметр = {1} ", Ploshadiob(a, out per), per);
+			Console.ReadKey();
+		}
+	}
+}
